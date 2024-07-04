c1b24514e353b57da7b37691570d00aa0c1bc52f
Mohamed Saadh <hellosaadh.personal@gmail.com>
2024-07-04 13:02:02 +0530

    SignIn SignUp Auth Api Created

--- backend-developer/django/grogin/src/grogin/api/v1/auth/views.py
+++ backend-developer/django/grogin/src/grogin/api/v1/auth/views.py
@@ -1,3 +1,6 @@
+import requests
+import json
+
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -15,10 +18,39 @@ def create(request):
 
     if not User.objects.filter(username=username).exists():
         user = User.objects.create_user(username=username, email=email, password=password)
-        response_data = {
-            "status_code": 200,
-            "data": "User created successfully",
+
+        headers = {
+            "Content-Type": "application/json",
         }
+
+        data = {
+            "username": username,
+            "email": email,
+            "password": password
+        }
+
+        protocol = "http://"
+        if request.is_secure():
+            protocol = "https://"
+            
+        host = request.get_host()
+
+        url = protocol + host + "/api/v1/auth/token/"
+
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+
+        if response.status_code == 200:
+            response_data = {
+            "status_code": 200,
+            "data": response.json(),
+            "message": "User created successfully",
+        }
+        else:
+            response_data = {
+            "status_code": 400,
+            "data": "User creation failed",
+        }
+            
     else:    
         response_data = {
             "status_code": 400,
