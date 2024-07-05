857ccb0e2fc9d87aef971e6504bf0efaca5d5303
Mohamed Saadh <hellosaadh.personal@gmail.com>
2024-07-05 18:44:39 +0530

    search api created but fully created

--- backend-developer/django/grogin/src/grogin/api/v1/products/serializers.py
+++ backend-developer/django/grogin/src/grogin/api/v1/products/serializers.py
@@ -1,18 +1,29 @@
-from rest_framework.serializers import ModelSerializer
-from products.models import Product
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from products.models import Product, ByColor, ByBrand, ByProductStatus
 
 from rest_framework import serializers
 
-class ProductSerializer(ModelSerializer):
+class ByColorSerializer(ModelSerializer):
+    class Meta:
+        model = ByColor
+        fields = ('color',)
 
+class ByBrandSerializer(ModelSerializer):
+    class Meta:
+        model = ByBrand
+        fields = ('brand',)
+
+class ByProductStatusSerializer(ModelSerializer):
+    class Meta:
+        model = ByProductStatus
+        fields = ('product_status',)
+
+class ProductSerializer(ModelSerializer):
     category = serializers.StringRelatedField()
+    colors = ByColorSerializer(source='bycolor_set', many=True)
+    brands = ByBrandSerializer(source='bybrand_set', many=True)
+    statuses = ByProductStatusSerializer(source='byproductstatus_set', many=True)
 
     class Meta:
-        fields = ("id", "product_title", "product_img", "product_price", "category")
         model = Product
-
-    def get_category(self, instance):
-        return instance.category.category_title
-    
-
-    
+        fields = ("id", "product_title", "product_img", "product_price", "category", "colors", "brands", "statuses")
--- backend-developer/django/grogin/src/grogin/api/v1/products/views.py
+++ backend-developer/django/grogin/src/grogin/api/v1/products/views.py
@@ -1,17 +1,22 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
 from products.models import Product
-
 from api.v1.products.serializers import ProductSerializer
 
 @api_view(['GET'])
 def products(request):
+    instances = Product.objects.all()  # Fetch all Product instances
 
-    instances = Product.objects.filter()
+    q = request.GET.get("q")
+        # instances = instances.filter(product_title__icontains=q)
+        # instances = instances.filter(category__category_title__icontains=q)
+        # instances = instances.filter(category__in=ids)
+    if q:
+        instances = instances.filter(colors__color__icontains=q)
+
     serializer = ProductSerializer(instances, many=True)
-    Response_data = {
+    response_data = {
         "status": 200,
         "data": serializer.data
     }
-    return Response(Response_data)
+    return Response(response_data)
