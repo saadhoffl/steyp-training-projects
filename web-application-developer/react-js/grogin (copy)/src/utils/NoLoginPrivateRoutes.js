8b35713bd22f4e8c728d66ca2454ec86f6be9939
Mohamed Saadh <hellosaadh.personal@gmail.com>
2024-07-18 09:23:37 +0530

    added and deleted some projects

--- web-application-developer/react-js/grogin (copy)/src/utils/NoLoginPrivateRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Outlet, Navigate } from "react-router-dom";
-
-const NoLoginPrivateRoutes = () => {
-  let auth = localStorage.getItem("user_data");
-
-  return !auth ? <Outlet /> : <Navigate to="/" />;
-};
-
-export default NoLoginPrivateRoutes;
--- web-application-developer/react-js/grogin (copy)/src/utils/PrivateRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Outlet, Navigate } from "react-router-dom";
-
-const PrivateRoutes = () => {
-  let auth = localStorage.getItem("user_data");
-
-  return auth ? <Outlet /> : <Navigate to="/signin" />;
-};
-
-export default PrivateRoutes;
