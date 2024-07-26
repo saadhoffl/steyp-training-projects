053f08306f42de579f472aa9f8ec5049e1c33fb5
Mohamed Saadh <hellosaadh.personal@gmail.com>
2024-07-26 12:07:50 +0530

    Navbar successfully completed

--- web-application-developer/next-js/exclusive/app/components/Navbar/Navbar.js
+++ web-application-developer/next-js/exclusive/app/components/Navbar/Navbar.js
@@ -1,41 +1,40 @@
 import React from "react";
 import style from "./Navbar.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import {
+  faMagnifyingGlass,
+  faHeart,
+  faCartShopping,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
   return (
-    <section className={style.container}>
-      <div className={style.left}>
-        <span className={style.logo}>Exclusive</span>
-      </div>
-      <div className={style.middle}>
-        <ul className={style.navbar}>
-          <li className={style.navitem}>Home</li>
-          <li className={style.navitem}>Contact</li>
-          <li className={style.navitem}>About</li>
-          <li className={style.navitem}>Signup</li>
-        </ul>
-      </div>
-      <div className={style.right}>
-        <div className={style.search_bar_container}>
-          <input
-            className={style.search_bar}
-            type="text"
-            placeholder="Search"
-          />
-          <span className={style.search_icon}>
-            <FontAwesomeIcon icon={faChevronDown} />
-          </span>
+    <section className={style.top_container}>
+      <section className={style.container}>
+        <div className={style.left}>
+          <span className={style.logo}>Exclusive</span>
         </div>
-
-        <span className={style.user_wishlist}>
-          <FontAwesomeIcon icon={faChevronDown} />
-        </span>
-        <span className={style.cart_icon}>
-          <FontAwesomeIcon icon={faChevronDown} />
-        </span>
-      </div>
+        <div className={style.middle}>
+          <ul className={style.navbar}>
+            <li className={(style.navitem, style.active)}>Home</li>
+            <li className={style.navitem}>Contact</li>
+            <li className={style.navitem}>About</li>
+            <li className={style.navitem}>Signin</li>
+          </ul>
+        </div>
+        <div className={style.right}>
+          <div className={style.search_bar_container}>
+            <input
+              className={style.search_bar}
+              type="text"
+              placeholder="What are you looking for?"
+            />
+            <FontAwesomeIcon className={style.icon} icon={faMagnifyingGlass} />
+          </div>
+          <FontAwesomeIcon className={style.icon} icon={faHeart} />
+          <FontAwesomeIcon className={style.icon} icon={faCartShopping} />
+        </div>
+      </section>
     </section>
   );
 }
